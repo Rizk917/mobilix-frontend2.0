b0b136178f054300479db84815a7f0eeb667d52a
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-02-08 18:58:50 +0200

    adjusted footer, and phone details

--- src/components/PhoneDetails.jsx
+++ src/components/PhoneDetails.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import DeskFooter from './deskfooter';
 import DeskHeader from './deskheader';
 
-function PhoneDetails() {
+function PhoneDetails(props) {
   const [open, setOpen] = useState(true);
   const { id } = useParams();
   const [phone, setPhone] = useState({});
@@ -22,15 +22,15 @@ function PhoneDetails() {
     <div className="phone-details">
       <DeskHeader open={open} setOpen={setOpen} />
       <div className='details-div'>
-      <img src={`http://localhost:5000/${phone.image}`} alt={phone.phoneModel} />
-      <div className='inform'>
-      <h3>{phone.phoneModel}</h3>
-      <h3>{phone.vendor}</h3>
-      <p>description: {phone.description}</p>
-      <p>Camera: {phone.camera}</p>
-      <p>body: {phone.body}</p>
-      <p>Release date: {phone.prodDate}</p>
-      </div>
+        <img src={`http://localhost:5000/${phone.image}`} alt={phone.phoneModel} />
+        <div className='inform'>
+          <h3>{phone.phoneModel}</h3>
+          <h3>{phone.vendor}</h3>
+          <p>description: {phone.description}</p>
+          <p>Camera: {phone.camera}</p>
+          <p>body: {phone.body}</p>
+          <p>Release date: {phone.prodDate}</p>
+        </div>
       </div>
       <DeskFooter setOpen={setOpen} />
     </div>
