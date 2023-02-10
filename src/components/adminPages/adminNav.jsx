e8f8fbe5ad0d938ebf50f5f52c3eabd80f22a01c
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2023-02-10 13:24:08 +0200

    login

--- src/components/adminPages/adminNav.jsx
+++ src/components/adminPages/adminNav.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './adminNav.css'
-import Dashborad from "./Dashboard";
 import AdminContactUs from "./contactUsadmin";
 
 function AdminNav() {
+    const logOut = () => {
+        window.location.href = "/";
 
+        window.localStorage.clear();
+        localStorage.removeItem('token');
+      };
     return (
         <div className="adnav">
             <ul className="ul-adminNav">
@@ -14,6 +18,7 @@ function AdminNav() {
                     <li className="nav-item"><a href="/AdminContactUs">contact us</a></li>
                     <li className="nav-item"><a href="/Adminarticle">article</a></li>
                     <li className="nav-item"><a href="/AdminPhonedata">phones</a></li>
+                    <li className="nav-item"><a href="/" onClick={logOut}>Logout</a></li>
                 </div>
             </ul>
         </div>
--- src/components/log-in-pop.jsx
+++ src/components/log-in-pop.jsx
@@ -1,50 +1,12 @@
-import axios from "axios";
 import React, { useState } from "react";
 
 const LgiPop = () => {
-    // const [formData, setFormData] = useState({
-    //     email: "",
-    //     password: ""
-    // });
-
-
-    // const { email, password } = formData;
-
-    // const handleChange = (e) => {
-    //     setFormData({ ...formData, [e.target.name]: e.target.value });
-    // };
-
-    
-const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-
-	async function loginUser(event) {
-		event.preventDefault()
-
-		const response = await fetch('http://localhost:1337/api/login', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				email,
-				password,
-			}),
-		})
-
-		const data = await response.json()
-
-		if (data.user) {
-			localStorage.setItem('token', data.user)
-			alert('Login successful')
-			window.location.href = '/dashboard'
-		} else {
-			alert('Please check your username and password')
-		}
-	}
-
-
+  
     const [error, setError] = useState("");
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -53,19 +15,37 @@ const [email, setEmail] = useState('')
         } else if (!password) {
             setError("Password is required");
         } else {
-            setError("");
-            try {
-                const newUser = { email, password };
-                await axios.post("YOUR_API_ENDPOINT_FOR_LOGIN", newUser);
-                // Handle successful login, e.g. by storing the token in local storage
-                // ...
-            } catch (err) {
-                console.error(err.response.data);
-                // Handle login error, e.g. by displaying an error message
-                // ...
-            }
-        }
-    };
+            fetch("http://localhost:5000/user/login", {
+                method: "POST",
+                crossDomain: true,
+                headers: {
+                  "Content-Type": "application/json",
+                  Accept: "application/json",
+                  "Access-Control-Allow-Origin": "*",
+                },
+                body: JSON.stringify({
+                  email,
+                  password,
+                }),
+              })
+                .then((res) => res.json())
+                .then((data) => {
+                  console.log(data, "userRegister");
+                  
+                  if(data.status=400){
+                    {
+                        setError("Incorrect credentials")
+                      }
+                  }
+                  if ((data.status = 201)) {
+                    alert("login successful");
+                    window.localStorage.setItem("token", data.token);
+                    window.localStorage.setItem("loggedIn", true);
+          
+                    window.location.href = "/dashboard";
+                  }
+                });
+            }};
 
     return (
         <div className="pop-up-login">
@@ -79,7 +59,6 @@ const [email, setEmail] = useState('')
                         name="email"
                         value={email}
                         placeholder="Your email"
-                        // onChange={handleChange}
                         onChange={(e) => setEmail(e.target.value)}
                     />
                     {error === "Password is required" && <p className="error">{error}</p>}
@@ -88,9 +67,9 @@ const [email, setEmail] = useState('')
                         name="password"
                         value={password}
                         placeholder="Your password"
-                        // onChange={handleChange}
                         onChange={(e) => setPassword(e.target.value)}
                     />
+
                     <button className="formlg-sbmt" value="login" type="submit">Submit</button>
                 </form>
                 <div className="arrow-back">
