6e7d745b96815b9660ac8974190fd86d5e99029c
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2023-02-11 21:20:23 +0200

    phones

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom'  ;
-import './Style.css'
-import Home from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Style.css";
+import Home from "./App";
+import App from "./App";
 
+// ReactDOM.render(<Home />, document.getElementById("root"));
 
-ReactDOM.render(
-    <Home/>
-,document.getElementById('root'));
-
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
