5299355eca0e845fe6ee2da4d22dc1c06f705c2d
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2023-02-11 14:13:28 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { Redirect, Router } from "react-router";
 import { BrowserRouter, Route, Routes, } from "react-router-dom";
 import About from "./components/About_us";
 import Home from "./components/main";
-import ContactUs from "./components/contactUs";
 import NewsP from "./components/News-page";
 import LogIn from "./components/log-in-phone";
 import LgiPop from "./components/log-in-pop";
