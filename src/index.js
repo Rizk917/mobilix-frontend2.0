98c25d26ecca79ba24575589a09f809ff06544de
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2023-02-11 18:14:41 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'  ;
 import './Style.css'
-import Home from './App';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './components/main';
 
-
-ReactDOM.render(
-    <Home/>
-,document.getElementById('root'));
-
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ 
+  <React.StrictMode>
+    <BrowserRouter>
+    <Home />
+    </BrowserRouter>
+  </React.StrictMode>
+ 
+);
