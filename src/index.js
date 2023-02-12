c068da24272383d1f6b08ec1036e2db34a40aed6
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2023-02-12 19:56:30 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./Style.css";
-import Home from "./App";
+// import Home from "./App";
 import App from "./App";
 
 // ReactDOM.render(<Home />, document.getElementById("root"));
